081a6dced3ba8188deee9b06a5b8416e37074ef0
Ivan Shim <ihshim523@gmail.com>
2026-05-27 16:17:42 -0400

    Use shared @freeqone/ui-theme package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ThemeToggle from "./ThemeToggle";
+import ThemeToggle from "@freeqone/ui-theme/ThemeToggle";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
